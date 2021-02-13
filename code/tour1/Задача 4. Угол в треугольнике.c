d0c366056991e6f676d014959bc546b3f3f9526b
uchansansan <uchansansan@gmail.com>
2021-02-13 11:49:00 +0700

    Update Задача 4. Угол в треугольнике.c

--- code/tour1/Задача 4. Угол в треугольнике.c
+++ code/tour1/Задача 4. Угол в треугольнике.c
@@ -1,40 +1,49 @@
+#define _USE_MATH_DEFINES
 #include <stdio.h>
 #include <math.h>
-#define E 0.000000000001
+#define E 0.00000000001
 
+double msin(double xa,double ya,double xb,double yb,double xc,double yc){
 
-double Arccos(double x, double eps){
-  double arccos=x;
-  double a=x;
-  int i=1;
-  while(a > eps){
-    a*=x*x*(2*i-1)*(2*i-1)/((2*i+1)*2*i);
-    arccos+=a;
-    i++;
-  }
-  return M_PI/2-arccos;
-}
-
-
-double co(double xa,double ya,double xb,double yb,double xc,double yc){
+  double a = sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya));
+  double b = sqrt((xc-xa)*(xc-xa)+(yc-ya)*(yc-ya));
+  double c = sqrt((xb-xc)*(xb-xc)+(yb-yc)*(yb-yc));
   double x1 = xb-xa;
   double y1 = yb-ya;
   double x2 = xc-xa;
   double y2 = yc-ya;
 
-  return (x1*x2+y1*y2)/(sqrt(x1*x1+y1*y1)*sqrt(x2*x2+y2*y2));
-
-}
-int main() {
-    int n;
-    scanf("%d",&n);
-
-    double xa,ya,xb,yb,xc,yc;
-    printf("\n");
-    for(int i = 0; i < n; i++){
-        scanf("%lf%lf%lf%lf%lf%lf",&xa,&ya,&xb,&yb,&xc,&yc);
-        printf("%0.20g\n", (Arccos(co(xa,ya,xb,yb,xc,yc),E)*180)/M_PI);
-        //printf("%0.20Lf\n",co(xa,ya,xb,yb,xc,yc));
-    }
+  if(a==0 || b==0 || c==0)
     return 0;
+  
+  double p = (a+b+c)/2.00000000000000000000000000;
+  double s = fabs(x1*y2-x2*y1)/2.00000000000000000000000000;
+  double sin = (2*s)/(a*b);
+
+  return sin;
+}
+
+double mcos(double xa,double ya,double xb,double yb,double xc,double yc){
+  double x1 = xb-xa;
+  double y1 = yb-ya;
+  double x2 = xc-xa;
+  double y2 = yc-ya;
+  double chis = x1*x2 + y1*y2;
+  double znam = sqrt((x1*x1+y1*y1)*(x2*x2+y2*y2));
+  double cos = chis/znam;
+  return cos;
+}
+
+int main(void) {
+  int n;
+  scanf("%d",&n);
+  double xa, ya, xb, yb, xc, yc;
+
+  for(int i = 0; i < n; i++){
+
+    scanf("%lf%lf%lf%lf%lf%lf", &xa, &ya, &xb, &yb, &xc, &yc);
+
+    printf("%.20g\n", atan2(msin(xa, ya, xb, yb, xc, yc),mcos(xa, ya, xb, yb, xc, yc))*180/M_PI);
+  }
+  return 0;
 }
